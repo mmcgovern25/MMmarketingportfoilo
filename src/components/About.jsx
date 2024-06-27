b620cb9b329bd5b4e7738a4fa9e26dc7271f9e4e
Matthew McGovern <mattmcg37@gmail.com>
2024-06-27 07:57:00 -0300

    part 1 save

---
+++ src/components/About.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { services } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
+import { Tilt } from "react-tilt";
+
+const ServiceCard = ({ index, title, icon }) => (
+  <Tilt className='hidden md:flex xs:w-[240px] w-full'>
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+    >
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+      >
+        <img
+          src={icon}
+          alt='web-development'
+          className='h-56 rounded-[20px] object-contain'
+        />
+
+        <h3 className='text-white text-[20px] font-bold text-center'>
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  </Tilt>
+);
+
+const About = () => {
+  return (
+    <>
+
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+      >
+        Who am I? Well, from a professional stand point, I am a skilled web developer with already two roles under my belt of prior experience, along with a full stack intensive Web Development bootcamp to go along with it. Outside of studying & working, I'm also a real person with other interests like anyone else. Below you can get to know more of the personal side of me as well.
+      </motion.p>
+
+      <div className='mt-20 flex flex-wrap gap-10'>
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(About, "about");
